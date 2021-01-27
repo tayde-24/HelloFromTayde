4c2b8250b8d1b4d9b34f1514701f24637179d202
NathanR-Git <77987757+NathanR-Git@users.noreply.github.com>
2021-01-26 20:05:35 -0700

    Added my name to the project.

--- src/luevano/Main.java
+++ src/luevano/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Tayde A. Luevano.");
+        System.out.println("Hello Tayde, my name is Nathan.");
     }
 }
